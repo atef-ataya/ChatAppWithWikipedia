d436ae68f086fd006288aab01d5ce914d5878d4a
Atef Ataya <atef.ataya@gmail.com>
2024-02-09 23:35:19 +0400

    Adding Function to: Calculating cost chat with wikipedia

--- chatapp.py
+++ chatapp.py
@@ -17,20 +17,20 @@ def split_text_into_chunks(data, chunk_size=256, chunk_overlap=20):
     chunks = text_splitter.split_documents(data)
     return chunks
 
+# Function to create the embedding from chunks and return the vector store
 def create_embeddings(chunks):
     embeddings = OpenAIEmbeddings()
     vector_store = Chroma.from_documents(chunks, embeddings)
     return vector_store
 
+# Function to calculate the cost of embeddings
 def calculate_and_display_embedding_cost(texts):
     import tiktoken
     enc = tiktoken.encoding_for_model('text-embedding-ada-002')
     total_tokens = sum([len(enc.encode(page.page_content)) for page in texts])
-    print(f'Total Tokens: {total_tokens}')
-    print(f'Embedding Cost in USD:{total_tokens / 1000 * 0.0004:.6f}')
     return total_tokens, total_tokens / 1000 * 0.0004
 
-
+# Function that allows us to chat with Wikipedia
 def chat_app_with_wikipedia(vector_store, query, chat_history=[], k=3):
     from langchain.chains import ConversationalRetrievalChain
     from langchain_openai import ChatOpenAI
@@ -43,3 +43,10 @@ def chat_app_with_wikipedia(vector_store, query, chat_history=[], k=3):
     chat_history.append((query, result['answer']))
 
     return result, chat_history
+
+# Function to clear the history of chat from the session state
+def clear_history():
+    if 'history' in st.session_state:
+        del st.session_state['history']
+
+# Add application entry point
